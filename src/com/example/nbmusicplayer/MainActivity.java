b32c752aaf5c6022b5a7d0d53d50ba5e498215f6
yysnow <978510569@qq.com>
2014-09-09 15:53:48 +0800

    test1

--- src/com/example/nbmusicplayer/MainActivity.java
+++ src/com/example/nbmusicplayer/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends Activity {
+	private int test;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
